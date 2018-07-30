61af9ce0bcc02dbaf141ddd0475323ba6d07d2cb
Ismelito Malonzo <idmlnz@yahoo.com>
2018-07-29 17:29:13 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# spring-boot-microservice
+Tutorial: https://in28minutes1.teachable.com/courses/enrolled/257263
+
+
---
+++ src/main/java/com/in28minutes/springboot/microservice/eureka/naming/server/springbootmicroserviceeurekanamingserver1/SpringBootMicroserviceEurekaNamingServer1Application.java
@@ -0,0 +1,12 @@
+package com.in28minutes.springboot.microservice.eureka.naming.server.springbootmicroserviceeurekanamingserver1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroserviceEurekaNamingServer1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroserviceEurekaNamingServer1Application.class, args);
+	}
+}
